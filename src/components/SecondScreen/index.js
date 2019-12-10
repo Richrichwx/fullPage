e69c3797a99ce89e6835c7809b4be3d3a4276ffa
Anna <guseva.annawx@gmail.com>
2019-12-10 17:14:23 +0100

    fix

--- src/components/SecondScreen/index.js
+++ src/components/SecondScreen/index.js
@@ -3,57 +3,52 @@ import ReactFullpage from '@fullpage/react-fullpage';
 import 'fullpage.js/vendors/scrolloverflow';
 
 const SecondPage = () => {
-    return (
-      <ReactFullpage
-        scrollOverflow={true}
-        sectionsColor={["white"]}
-        render={() => {
-          return (
-            <div id="fullpage-wrapper">
-              <div className="section">
-                <h3>First page</h3>
-              </div>
-              <div className="section">
-                <div className="screenContainer">
-                  <div className="header">
-                    <div className="headerLogo">
-                      <img src="https://www.inx.co/static/icons/logoWhite.png" alt="" className="logo"/>
-                    </div>
-                    <div className="navBar">
-                      <div className="navAboutWrapper">
-                        <div className="navAbout">
-                          About Us
-                        </div>
+  return (
+    <ReactFullpage
+      scrollOverflow={ true }
+      sectionsColor={ ['white'] }
+      render={ () => {
+        return (
+          <div id="fullpage-wrapper">
+            <div className="section">
+              <h3>First page</h3>
+            </div>
+            <div className="section">
+              <div className="screenContainer">
+                <div className="header">
+                  <div className="headerLogo">
+                    <img src="https://www.inx.co/static/icons/logoWhite.png" alt="" className="logo"/>
+                  </div>
+                  <div className="navBar">
+                    <div className="navAboutWrapper">
+                      <div className="navAbout">
+                        About Us
                       </div>
-                      <div className="navJoinWrapper">
-                        <div className="navJoin">
-                          Join Us
-                        </div>
+                    </div>
+                    <div className="navJoinWrapper">
+                      <div className="navJoin">
+                        Join Us
                       </div>
                     </div>
                   </div>
+                </div>
+                <div className="contentContainer">
                   <div className="content">
                     <h1 className="animated textContent">Introducing the New Regulated Standard</h1>
                     <div className="animatedSection sectionWrapper">
                       <div className="sectionScreen">
-                        <div className="contentIcon">
-                        </div>
                         <div className="sectionInfo">
                           <span>Regulatory Oversight</span>
                           <p>Built according to SEC, FINRA and EU regulations</p>
                         </div>
                       </div>
                       <div className="sectionScreen">
-                        <div className="contentIcon">
-                        </div>
                         <div className="sectionInfo">
                           <span>World-Class Safety</span>
                           <p>The highest security standards to ensure funds are protected</p>
                         </div>
                       </div>
-                      <div className="sectionScreen exclusion">
-                        <div className="contentIcon">
-                        </div>
+                      <div className="sectionScreen">
                         <div className="sectionInfo">
                           <span>A Broad Crypto Trading Offering</span>
                           <p>Aiming to establish a spot trading suite for cryptocurrencies and security tokens</p>
@@ -61,13 +56,15 @@ const SecondPage = () => {
                       </div>
                     </div>
                   </div>
-                  </div>
+                  <div className="chartImage"></div>
                 </div>
               </div>
-          );
-        }}
-      />
-    );
+            </div>
+          </div>
+        );
+      } }
+    />
+  );
 };
 
 export default SecondPage;
